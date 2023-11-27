a75913b3c80a6db473b86e0153deedf8a9d8ffc9
anav5704 <anavkki@gmail.com>
2023-11-27 22:18:51 +1200

    Welcome Message

---
+++ lesson-2/Welcome.py
@@ -0,0 +1,10 @@
+line01 = "********************"  # Header / Footer
+line02 = "*                  *"
+line03 = "*     Welcome!     *"
+
+print("")
+print(line01)
+print(line02)
+print(line03)
+print(line02)
+print(line01)
