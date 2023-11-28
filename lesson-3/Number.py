ed17e14cbfb7826d43947674c1b5522b836b9e16
anav5704 <anavkki@gmail.com>
2023-11-28 20:02:11 +1200

    Operators

---
+++ lesson-3/Number.py
@@ -0,0 +1,10 @@
+number = 100
+
+# Basic else if
+if number > 50: 
+    print("Yesir")
+else: 
+    print("Nah Uh")
+
+# Goofy ah ternary operator syntax
+print("Yessir") if number > 50 else print("Nah Uh") 
---
+++ lesson-3/Operators.py
@@ -0,0 +1,19 @@
+fname = "Anav"
+lname = "Chand"
+age = 19
+
+print(fname, lname, age)
+
+print(20 / 3) # Normal divison
+print(20 // 3) # Floor  divison
+
+print(2 * 4) # Multiplication
+print(2 ** 4) # Exponent
+
+print(1 == 1) # I miss my ===
+print(2 != 1)
+
+not True
+
+True and True # &&
+False or True # ||
