9ed841547af9ea56891107cd7c676f5648f3458e
anav5704 <anavkki@gmail.com>
2023-12-06 07:03:00 +1200

    Modules

---
+++ lesson-14/Modules.py
@@ -0,0 +1,13 @@
+from enum import Enum
+import random as rdm
+from math import tau
+import sys
+
+print(tau)
+
+for item in dir(rdm):
+    print(item)
+
+print(sys.__name__)
+
+sys.exit("Quitting...")
