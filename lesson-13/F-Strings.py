16f8d6c52b2f3602d967a07f86ff2aaf3c991ae9
anav5704 <anavkki@gmail.com>
2023-12-05 20:13:34 +1200

    F Strings

---
+++ lesson-13/F-Strings.py
@@ -0,0 +1,16 @@
+name = "Anav"
+
+message = "\n %s has a laptop" %(name)
+print(message)
+
+message = "\n {} has a laptop".format(name)
+print(message)
+
+message = "\n {0} has a laptop".format(name)
+print(message)
+
+message = "\n {name} has a laptop".format(name = name)
+print(message)
+
+message =  f"\n {name} has a laptop"
+print(message)
