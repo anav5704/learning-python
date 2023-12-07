8e9c56dbc495d04409906fa06da02770c355a4ac
anav5704 <anavkki@gmail.com>
2023-12-07 12:15:32 +1200

    Pip and Venv

---
+++ lesson-20/Weather.py
@@ -0,0 +1,18 @@
+from pprint import pprint
+import requests
+import os
+
+
+def getWeather():
+    print("\n +++ Current Weather Conditions +++")
+    
+    city = input("\n Enter a City name: ")
+    url = f"https://api.openweathermap.org/data/2.5/weather?appid=d65fa1b1e17b2096eef9ee9a53fcb75e&q={city}&units=metric"
+
+    data = requests.get(url).json()
+
+    print(f'\nCurrent weather for {data["name"]}:')
+    print(f'\nThe temp is {data["main"]["temp"]:.1f}°')
+    print(f'\n{data["weather"][0]["description"].capitalize()} and feels like {data["main"]["feels_like"]:.1f}°\n')
+    
+getWeather()
